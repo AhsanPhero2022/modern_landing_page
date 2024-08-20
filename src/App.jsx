f69b94847207ad12c9cab4d64d4662c4808bec8b
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-08-21 01:33:31 +0600

    section component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">hello world</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
 
       <ButtonGradient />
     </>
